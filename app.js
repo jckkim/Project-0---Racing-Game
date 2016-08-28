bcdabb07ef4b0ed01777adf2d1a26a68e09833bd
jasonkim <jckkim@yahoo.com>
2016-08-28 14:22:22 -0700

    moves cars

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 console.log('Testing Consolelog')
+
+$(this).on("keydown", function(e){
+  if (e.keyCode == 65) {
+    $('#car1').css({"padding-left": "+=50px"})
+    console.log("You pressed A!");
+  }
+    else if (e.keyCode == 74) {
+      $('#car2').css({"padding-left": "+=50px"})
+      console.log("You pressed J!");
+    }
+  });
 });
